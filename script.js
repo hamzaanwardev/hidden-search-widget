51024f86be91b7a991e674cf9d004874f08f191c
Hamza Anwar <hamzaanwardev@gmail.com>
2021-08-13 02:33:11 +0500

    Added Comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
-
+// on click toggle class active
 search.addEventListener("click", () => {
     search.classList.toggle("active")
     input.focus()
